142703db7343567298aa46e0d30a004de847a15e
zhangzhenxiao <neozzx@163.com>
2018-04-23 23:01:41 +0800

    upload

---
+++ jiuzhou/jiuzhou/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  jiuzhou
+//
+//  Created by 震霄 张 on 2018/4/23.
+//  Copyright © 2018年 neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ jiuzhou/jiuzhou/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  jiuzhou
+//
+//  Created by 震霄 张 on 2018/4/23.
+//  Copyright © 2018年 neo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
